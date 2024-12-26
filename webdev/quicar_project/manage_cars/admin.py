e518e9f0291c5963c6c843f77d8bd073baa12f71
Hitesh-Potla <138430265+Hitesh-Potla@users.noreply.github.com>
2024-12-26 17:37:43 +0530

    Delete webdev/quicar_project directory

--- webdev/quicar_project/manage_cars/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RentalCar,Transaction
-
-# Register your models here.
-admin.site.register(RentalCar)
-admin.site.register(Transaction)
--- webdev/quicar_project/user_auth/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserAuthConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "user_auth"
-    def ready(self):
-        import user_auth.signals  # Ensure the signals are loaded
-
